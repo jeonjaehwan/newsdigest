cf3df9baffd6b5f0debeaed16ad6ac28dd978bdf
jeonjaehwan <jeonjaehwan12@naver.com>
2024-08-28 21:15:47 +0900

    jenkins 사용

--- backend/src/main/java/com/newsdigest/newsdigest/config/QuartzConfig.java
+++ backend/src/main/java/com/newsdigest/newsdigest/config/QuartzConfig.java
@@ -12,6 +12,7 @@ import org.springframework.scheduling.quartz.JobDetailFactoryBean;
 public class QuartzConfig {
 
 
+
     @Bean
     public JobDetailFactoryBean jobDetailFactoryBean() {
         JobDetailFactoryBean factoryBean = new JobDetailFactoryBean();
